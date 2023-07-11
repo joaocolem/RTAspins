47a18fc1dc40d1c3d76697930df03ec4e996cb25
joaocolem <105292741+joaocolem@users.noreply.github.com>
2023-07-11 13:53:39 -0300

    commit2

---
+++ src/Controller/ScreenDetection/ImgToTxt/ImgToText.java
@@ -0,0 +1,14 @@
+package Controller.ScreenDetection.ImgToTxt;
+import Controller.ScreenDetection.Screenshot;
+
+import java.io.PrintWriter;
+
+public class ImgToText {
+
+    public static void main(String[] args) {
+        
+        Screenshot.captureScreenshot(380, 420, 72, 23, "C:\\imge\\screen4.png");
+
+
+        }
+}
